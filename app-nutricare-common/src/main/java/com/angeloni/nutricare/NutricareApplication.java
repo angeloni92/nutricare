cc32620130c7465b6f49eb48790308f43969b3f5
angeloni <andrea.angeloni92@yahoo.it>
2024-10-28 10:53:04 +0100

    configurazione completata

--- app-nutricare-common/src/main/java/com/angeloni/nutricare/NutricareApplication.java
+++ app-nutricare-common/src/main/java/com/angeloni/nutricare/NutricareApplication.java
@@ -2,8 +2,10 @@ package com.angeloni.nutricare;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.angeloni.nutricare")
 public class NutricareApplication {
 
 	public static void main(String[] args) {
